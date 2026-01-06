425476e91efed0e54a2875d80873e6504908cc14
Mario Ruby A <pius.marioruby@gmail.com>
2026-01-06 19:34:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -174,9 +174,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-<?php
-
-use Illuminate\Support\Facades\Route;
 
 // Temporary debug route - REMOVE IN PRODUCTION
 Route::get('/debug-css', function () {
